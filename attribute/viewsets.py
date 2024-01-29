0774adfee8f0e006ec1b9f99002694bcadc39185
Hamza <hamzach1288@gmail.com>
2024-01-29 13:16:17 +0500

    search (attribute)

--- attribute/viewsets.py
+++ attribute/viewsets.py
@@ -1,5 +1,7 @@
 from rest_framework.response import Response
 from rest_framework import viewsets
+
+from .filterset import AttributeFilterset
 from .models import Attribute
 from .serializer import AttributeHistorySerializer, AttributeSerializer
 
@@ -7,10 +9,11 @@ from .serializer import AttributeHistorySerializer, AttributeSerializer
 class AttributeViewset(viewsets.ModelViewSet):
     queryset = Attribute.objects.all()
     serializer_class = AttributeSerializer
+    filterset_class = AttributeFilterset
 
 
 class AttributeHistoryViewset(viewsets.ViewSet):
     def retrieve(self, request, pk=None):
-        queryset = Attribute.history.filter(id=pk)
+        queryset = Attribute.history.filter(code=pk)
         serializer = AttributeHistorySerializer(queryset, many=True)
         return Response(serializer.data)
