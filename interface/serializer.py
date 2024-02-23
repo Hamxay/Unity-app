7ddd1ff002c25d19c85461dde8cb381d82632dbc
Hamxay <hamzach1288@gmail.com>
2024-02-23 15:38:39 +0500

    interface dependence foreign key

--- interface/serializer.py
+++ interface/serializer.py
@@ -23,12 +23,6 @@ class InterfaceTypeDisplaySerializer(serializers.ModelSerializer):
         fields = ['code', 'name']
 
 
-class InterfaceDisplaySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = InterfaceType
-        fields = ['code', 'name']
-
-
 class InterfaceCategorySerializer(serializers.ModelSerializer):
     created_by = serializers.ReadOnlyField(source="created_by.get_full_name")
     updated_by = serializers.ReadOnlyField(source="created_by.get_full_name")
@@ -72,13 +66,8 @@ class InterfaceHistorySerializer(serializers.ModelSerializer):
 class InterfaceDependenceSerializer(serializers.ModelSerializer):
     created_by = serializers.ReadOnlyField(source="created_by.get_full_name")
     updated_by = serializers.ReadOnlyField(source="created_by.get_full_name")
+    interface_id = InterfaceDisplaySerializer()
 
     class Meta:
         model = InterfaceDependence
         fields = "__all__"
-
-
-class InterfaceDisplaySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Interface
-        fields = ['code', 'name']
